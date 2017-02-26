8ae33f09cef05f2f42d744cb19cb69f8671d7c78
Natalia.Nowak <natalia.rebilas@gmail.com>
2017-02-26 22:26:41 +0100

    getting products from database

--- src/main/java/app/ShopRestController.java
+++ src/main/java/app/ShopRestController.java
@@ -6,6 +6,10 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +19,24 @@ public class ShopRestController {
     @RequestMapping(value = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Product> products() {
         List<Product> result = new ArrayList<>();
-        result.add( new Product(1, "Okno balkonowe", "Oknoplast", 300));
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM PRODUCT;");
+            while (rs.next()) {
+                result.add(new Product(rs.getInt("id"), rs.getString("nazwa"), rs.getString("producent"), rs.getInt("cena")));
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         return result;
     }
